04078879998ab9ab7d4f82dfc1926b2ed6e60fbb
Tumen Bairov <bairovt@gmail.com>
2017-11-11 23:31:09 +0900

    first content

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Home from '@/components/Home'
+import About from '@/components/Pages/About'
+import Problem from '@/components/Pages/Problem'
+import Profile from '@/components/User/Profile'
+import Signin from '@/components/User/Signin'
+import Signup from '@/components/User/Signup'
 
 Vue.use(Router)
 
@@ -8,8 +13,40 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/problem',
+      name: 'Problem',
+      component: Problem
+    },
+    // {
+    //   path: '/meetup/new',
+    //   name: 'CreateMeetup',
+    //   component: CreateMeetup
+    // },
+    // {
+    //   path: '/meetups/:id',
+    //   name: 'Meetup',
+    //   props: true,
+    //   component: Meetup
+    // },
+    {
+      path: '/profile',
+      name: 'Profile',
+      component: Profile
+    },
+    {
+      path: '/signin',
+      name: 'Signin',
+      component: Signin
+    },
+    {
+      path: '/signup',
+      name: 'Signup',
+      component: Signup
     }
-  ]
+  ],
+  mode: 'history'
 })
