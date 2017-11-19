d9438262a2d515c0f0bc798c1b38263c4a7f6131
Tumen Bairov <bairovt@gmail.com>
2017-11-19 23:53:03 +0900

    content

--- src/components/Pages/About.vue
+++ src/components/Pages/About.vue
@@ -3,14 +3,28 @@
 		<v-layout row wrap>
 			<v-flex xs12>
 				<h5>О проекте</h5>
+				<!--<p>методом краудсорсинга.</p>-->
+				<p>Неравнодушным к судьбе нашего государства предлагаю принять участие в проекте</p>
+				<br>
+
+				<form>
+					<v-text-field>
+					</v-text-field>
+					<v-text-field>
+					</v-text-field>
+					<v-btn>Отправить</v-btn>
+				</form>
 			</v-flex>
 		</v-layout>
 	</v-container>
 </template>
 
 <script>
-	export default {
-	  computed: {
+	import VTextField from "vuetify/es5/components/VTextField/VTextField";
+
+  export default {
+    components: {VTextField},
+    computed: {
 	  }
 	}
 </script>
