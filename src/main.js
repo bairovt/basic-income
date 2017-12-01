a87355282a89329a65e468da3f5f7757def02c60
Tumen Bairov <bairovt@gmail.com>
2017-12-01 20:42:12 +0900

    a la carte

--- src/main.js
+++ src/main.js
@@ -1,14 +1,22 @@
 
 import Vue from 'vue'
-import Vuetify from 'vuetify'
 import './stylus/main.styl'
 import App from './App'
 import router from './router'
 import { store } from './store'
 import pageInDev from './components/Shared/PageInDev.vue'
 import AdditionDialog from './components/Shared/AdditionDialog.vue'
+import {
+    Vuetify,
+    VApp, VNavigationDrawer, VList, VIcon, VToolbar, VBtn, VGrid, VDialog, VCard, VAlert, VTextField
+} from 'vuetify'
 
-Vue.use(Vuetify)
+Vue.use(Vuetify, {
+  components: {
+    VApp, VNavigationDrawer, VList, VIcon, VToolbar, VBtn, VGrid, VDialog, VCard, VAlert, VTextField
+  }
+});
+
 Vue.component('app-page-in-dev', pageInDev)
 Vue.component('addition-dialog', AdditionDialog)
 
