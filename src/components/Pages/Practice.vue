487f6d6de157ee225e6b3b030b9ee80ca3390cce
Tumen Bairov <bairovt@gmail.com>
2017-12-24 00:44:28 +0900

    load news in Practice page

--- src/components/Pages/Practice.vue
+++ src/components/Pages/Practice.vue
@@ -4,14 +4,37 @@
 			<v-flex xs12>
 				<app-page-in-dev></app-page-in-dev>
 				<h5>Международная практика</h5>
+
+				<v-card class="mb-2" v-for="n in news" :key="n._id">
+					<v-card-title >
+						<a v-if="n.url" class="secondary--text" :href="n.url" :target="n.target">
+							{{ n.title }}
+						</a>
+					</v-card-title>
+				</v-card>
+
 			</v-flex>
 		</v-layout>
 	</v-container>
 </template>
 
 <script>
-	export default {
-	  computed: {
-	  }
+import axios from 'axios'
+// import openDialog from '@/utils/open-dialog'
+import axiosInst from '@/utils/axios-instance'
+
+export default {
+	data () {
+		return {
+			news: null
+		}
+	},
+	created: function () {
+		axiosInst.get('/api/load/news/in_practice')
+			.then(resp => {
+				this.news = resp.data.news
+			})
+			.catch(console.error)
 	}
+}
 </script>
